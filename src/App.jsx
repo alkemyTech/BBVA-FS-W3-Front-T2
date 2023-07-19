ec83845443d6fabca1663f03dfbdd1042385101b
Valentino Veralli <valentinoveralli@gmail.com>
2023-07-19 11:31:41 -0300

    layout component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import "./App.css";
-import NavBar from "./components/Header/NavBar";
-import navLinks from "./constants/navLinks";
-import Footer from "./components/Footer/Footer";
+import Layout from "./components/Layout/Layout";
 
 function App() {
   return (
-    <>
-      <NavBar navLinks={navLinks} />
-      <Footer></Footer>
-    </>
+    <Layout>
+    </Layout>
   );
 }
 
