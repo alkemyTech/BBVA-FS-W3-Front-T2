dfa290a7a9b483ef783dc97c791ef227698df730
Iara Pou <92182454+Iara-Pou@users.noreply.github.com>
2023-07-21 09:52:22 -0300

    Suma ruta de FixedTerm y LocalizationProvider

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,26 @@ import LoginForm from "./forms/LoginForm.jsx";
 import "./App.css";
 import RegisterForm from "./forms/RegisterForm.jsx";
 import Deposit from "./pages/Deposit/Deposit";
+import FixedTerm from "./pages/FixedTerm/FixedTerm";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 
 function App() {
   return (
     <div className="app-container">
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
       <NavBar navLinks={navLinks} />
       <Routes>
         {/* Posteriormente, al atributo element se le pasará la page que corresponda */}
         <Route path="/" />
-          <Route path="/Login" element={<LoginForm/>}/>
-          <Route path="/register" element={<RegisterForm/>} />
-          <Route path="/depositar" element={<Deposit />} />
-         <Route path="/transferir" />
-        <Route path="/plazo-fijo" />
+        <Route path="/Login" element={<LoginForm />} />
+        <Route path="/register" element={<RegisterForm />} />
+        <Route path="/depositar" element={<Deposit />} />
+        <Route path="/transferir" />
+        <Route path="/plazo-fijo" element={<FixedTerm />} />
       </Routes>
-      <Footer className="footer"/>
+      <Footer className="footer" />
+      </LocalizationProvider>
     </div>
   );
 }
