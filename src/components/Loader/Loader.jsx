d84a33d5c316b3d9b38f08c4c0c18690699c69dc
Iara Pou <92182454+Iara-Pou@users.noreply.github.com>
2023-07-25 14:21:25 -0300

    Crea componente Loader

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Loader.css";
+
+const Loader = () => {
+  return (
+    <div className="loadingContainer">
+      <img
+        src="../../../public/iconoAlkywallChico.png"
+        alt="icono de la empresa"
+        id="loadingIcon"
+      />
+      <img
+        src="../../../public/assets/iAzul.png"
+        alt="titulo de la empresa"
+        id="loadingTitle"
+      />
+    </div>
+  );
+};
+
+export default Loader;
