5109ef271bcb4b58ea99d7fda15e520dc9cca7be
Iara Pou <92182454+Iara-Pou@users.noreply.github.com>
2023-08-01 11:22:38 -0300

    Elimina console.log innecesarios

--- src/services/transferService.js
+++ src/services/transferService.js
@@ -8,11 +8,9 @@ export const sendARS = async (values) => {
     },
   };
   const host = "http://localhost:8080/transactions";
-  console.log(config);
   try {
     delete values.currency;
     const response = await axios.post(host + "/sendArs", values, config);
-    console.log(response);
     return response.data;
   } catch (error) {
     if (error.response && error.response.data && error.response.data.message) {
@@ -31,11 +29,9 @@ export const sendUSD = async (values) => {
     },
   };
   const host = "http://localhost:8080/transactions";
-  console.log(config);
   try {
     delete values.currency;
     const response = await axios.post(host + "/sendUsd", values, config);
-    console.log(response);
     return response.data;
   } catch (error) {
     if (error.response && error.response.data && error.response.data.message) {
